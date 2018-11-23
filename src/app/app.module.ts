fd2c7eae4a3d3b4021641ca09c66bf3d41de3ff0
rafysanchez <rafysanchez@hotmail.com>
2018-11-23 12:33:15 -0200

    adjs

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { UserComponent } from './user-component/user.component';
+import { UserService } from './services/user-service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
